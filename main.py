2e5712b0b8b1980f59bcaaf9b867e93ce2721cae
Miguel Nieto <migue418@hotmail.com>
2025-11-18 13:21:05 -0500

    updating cors rules again

--- main.py
+++ main.py
@@ -304,15 +304,12 @@ class CommentRead(BaseModel):
 app = FastAPI(title="AfroVice API", version="1.0.0")
 
 origins = [
-    "http://localhost:3000",
     "http://localhost:5173",
-    "http://127.0.0.1:3000",
-    "http://127.0.0.1:5173",
-    "https://afrovice.maaango.com/",
-    "https://afrovice-api.maaango.com/"
+    "https://afrovice.maaango.com",
+    "https://afrovice-api.maaango.com",
 ]
 
-vercel_regex = r"https://.*\.vercel\.app"
+vercel_regex = r"^https://.*\.vercel\.app$"
 
 app.add_middleware(
     CORSMiddleware,
