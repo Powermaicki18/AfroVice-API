61586e0c8608ac801e738dfb62fbaa639e78044a
Miguel Nieto <migue418@hotmail.com>
2025-11-18 13:16:06 -0500

    updating cors rules

--- main.py
+++ main.py
@@ -308,7 +308,8 @@ origins = [
     "http://localhost:5173",
     "http://127.0.0.1:3000",
     "http://127.0.0.1:5173",
-    "https://afrovice.maaango.com"
+    "https://afrovice.maaango.com/",
+    "https://afrovice-api.maaango.com/"
 ]
 
 vercel_regex = r"https://.*\.vercel\.app"
